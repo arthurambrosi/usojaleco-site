35cc8c70937d283b57ae4aac2bc512b8dc2a011c
arthurambrosi <arthurambrosi@gmail.com>
2026-02-12 14:51:12 -0300

    Update analytics-config.js

--- analytics-config.js
+++ analytics-config.js
@@ -3,7 +3,7 @@
 
   // Replace with your GA4 Measurement ID, e.g. "G-ABC123DEF4".
   global.USOJALECO_ANALYTICS_CONFIG = global.USOJALECO_ANALYTICS_CONFIG || {
-    measurementId: "290487225",
+    measurementId: "G-4E6S9EHGF6",
     debug: false
   };
 })(window);
