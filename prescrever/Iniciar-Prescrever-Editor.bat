97fc2b9e3f823d76c7a6ed85ba7ff40495dae64c
arthurambrosi <arthurambrosi@gmail.com>
2026-03-04 16:02:32 -0300

    Add files via upload

---
+++ prescrever/Iniciar-Prescrever-Editor.bat
@@ -0,0 +1,10 @@
+﻿@echo off
+setlocal
+for /f "tokens=5" %%P in ('netstat -ano ^| findstr ":8080" ^| findstr "LISTENING"') do taskkill /PID %%P /F >nul 2>&1
+cd /d "%~dp0"
+start "Prescrever" cmd /k "cd /d ""%~dp0"" && python -m http.server 8080"
+timeout /t 2 >nul
+start "" "http://localhost:8080/editor/?v=20260303-03"
+endlocal
+
+
